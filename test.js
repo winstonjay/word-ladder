7eb674bef30ea0ccb8d7afb0d6385d06c19fcd7d
winstonjay <karlsims.uk@gmail.com>
2019-06-13 11:43:45 +0100

    fixed tests

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const wl = require('./wordladder');
 
 describe('generate', function () {
     it('should be able to generate a puzzle at least 6 words long', function () {
-        assert.equal(wl.generate('hell', 6).length, 6);
+        assert.equal(wl.generate('wood', 6).length, 6);
     });
 });
 
@@ -12,7 +12,7 @@ describe('solve', function () {
     it('should find be able to find a valid solution', function () {
         assert.deepEqual(wl.solve('lent', 'wood', 6),
             ['lent', 'lend', 'fend', 'fond', 'food', 'wood']);
-    });S
+    });
     // should take a sec or 2
     it('should be able to solve all puzzles we can generate in our wordset', function () {
         let possible = 0;
